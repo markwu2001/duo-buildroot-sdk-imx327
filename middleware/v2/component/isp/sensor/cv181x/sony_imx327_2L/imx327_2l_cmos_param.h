87f1e6896121a43d2788c27c3352947dd8cb7f4c
Chun Qi (Mark) Wu <40764347+markwu2001@users.noreply.github.com>
2024-08-22 15:32:41 -0700

    Adapt imx327_2l_cmos_param.h
    - Increase MAC frequency from 200M to 400M because double clock speeds
    - Set MCLK frequency to 0 [CAMPLL_FREQ_NONE] 
    - Preconfigure lane id and pn swaps

--- middleware/v2/component/isp/sensor/cv181x/sony_imx327_2L/imx327_2l_cmos_param.h
+++ middleware/v2/component/isp/sensor/cv181x/sony_imx327_2L/imx327_2l_cmos_param.h
@@ -394,16 +394,16 @@ static ISP_CMOS_BLACK_LEVEL_S g_stIspBlcCalibratio = {
 
 struct combo_dev_attr_s imx327_2l_rx_attr = {
 	.input_mode = INPUT_MODE_MIPI,
-	.mac_clk = RX_MAC_CLK_200M,
+	.mac_clk = RX_MAC_CLK_400M,
 	.mipi_attr = {
 		.raw_data_type = RAW_DATA_12BIT,
-		.lane_id = {2, 1, 0, -1, -1},
-		.pn_swap = {1, 1, 1, 0, 0},
+		.lane_id = {5, 3, 4, -1, -1},
+		.pn_swap = {0, 0, 0, 0, 0},
 		.wdr_mode = CVI_MIPI_WDR_MODE_DOL,
 	},
 	.mclk = {
 		.cam = 0,
-		.freq = CAMPLL_FREQ_37P125M,
+		.freq = CAMPLL_FREQ_NONE,
 	},
 	.devno = 0,
 };
